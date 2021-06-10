f6a9b562a355824bcc1638204bf9a95b1641ef2e
Alan Fernandes <alanff3878@outlook.com>
2021-06-10 19:05:59 -0300

    react-05

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class Name extends Component{
+
+  state = {
+    pessoas:[
+      {
+        nome:'goku',
+        idade: 62,
+        
+      },{
+        nome:"wolverine",
+        idade:999999,
+        
+      },{
+        nome:"Vegeta",
+        idade:60,
+      
+      },{
+        nome:"kakashi",
+        idade: 40,
+        
+
+      },{
+        nome:"Boruto",
+        idade: 15,
+        
+      
+      },{
+        nome:"bruce-lee",
+        idade: 45,
+       
+      }
+    ]
+  };
+
+  render(){
+    return(
+      <div className = "map-box">
+      {this.state.pessoas.map((item) => (
+        <div className = "loca">
+          <h1 className ="nome">Meu nome é: {item.nome}</h1>
+           <h2 className ="idade">tenho: {item.idade} anos,</h2> 
+         
+        </div>
+      ))}
+      </div>
+
+
+
+    );
+  }
 }
 
-export default App;
+export default Name;
+
