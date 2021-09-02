8f56a47688623205c9b33e4801354215ad00ff81
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-09-02 16:59:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 
 import React, { Component } from 'react';
-
 class Name extends Component{
-
   state = {
     pessoas:[
       {
         nome:'goku',
         idade: 62,
-        
       },{
         nome:"wolverine",
         idade:999999,
-        
       },{
         nome:"Vegeta",
         idade:60,
-      
       },{
         nome:"kakashi",
         idade: 40,
-        
-
       },{
         nome:"Boruto",
         idade: 15,
-        
-      
       },{
         nome:"bruce-lee",
         idade: 45,
-       
       }
     ]
   };
-
   render(){
     return(
       <div className = "map-box">
@@ -42,13 +31,9 @@ class Name extends Component{
         <div className = "loca">
           <h1 className ="nome">Meu nome é: {item.nome}</h1>
            <h2 className ="idade">tenho: {item.idade} anos,</h2> 
-         
         </div>
       ))}
       </div>
-
-
-
     );
   }
 }
